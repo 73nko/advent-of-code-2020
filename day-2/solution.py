ab1f4e8cc0334906d238875fcbd17fd95453466f
73nko <apramos89@gmail.com>
2020-12-05 10:44:11 +0100

    Prepare day 3

--- day-2/solution.py
+++ day-2/solution.py
@@ -1,32 +1,32 @@
 def parseInput(input):
-    passwodsFile = open(input, 'r')
-    passwordsList = passwodsFile.read().splitlines()
+    passwods_file = open(input, 'r')
+    passwords_list = passwods_file.read().splitlines()
 
-    for pos, value in enumerate(passwordsList):
-        passwordsList[pos] = value.replace("-", " ").replace(":", "").split()
-        passwordsList[pos][0] = int(passwordsList[pos][0])
-        passwordsList[pos][1] = int(passwordsList[pos][1])
+    for pos, value in enumerate(passwords_list):
+        passwords_list[pos] = value.replace("-", " ").replace(":", "").split()
+        passwords_list[pos][0] = int(passwords_list[pos][0])
+        passwords_list[pos][1] = int(passwords_list[pos][1])
 
-    return passwordsList
+    return passwords_list
 
 
-def validPasswordsPart1(passwords):
-    validPasswordsCounts = 0
+def valid_passwords_part1(passwords):
+    valid_passwords_count = 0
     for line in passwords:
         if (line[0] <= line[3].count(line[2]) <= line[1]):
-            validPasswordsCounts += 1
-    return "Result puzzle 1: " + str(validPasswordsCounts) + " valid passwords"
+            valid_passwords_count += 1
+    return "Result puzzle 1: " + str(valid_passwords_count) + " valid passwords"
 
 
-def validPasswordsPart2(passwords):
-    validPasswordsCounts = 0
+def valid_passwords_part2(passwords):
+    valid_passwords_count = 0
     for line in passwords:
         if (line[3][line[0]-1] == line[2]) != (line[3][line[1]-1] == line[2]):
-            validPasswordsCounts += 1
-    return "Result puzzle 2: " + str(validPasswordsCounts) + " valid passwords"
+            valid_passwords_count += 1
+    return "Result puzzle 2: " + str(valid_passwords_count) + " valid passwords"
 
 
-passwordList = parseInput('passwords.txt')
+passwords_list = parseInput('passwords.txt')
 
-print(validPasswordsPart1(passwordList))
-print(validPasswordsPart2(passwordList))
+print(valid_passwords_part1(passwords_list))
+print(valid_passwords_part2(passwords_list))
